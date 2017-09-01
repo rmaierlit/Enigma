25829dd0d1e3d81149c77948fb5bf907562b5a60
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-09-01 15:54:43 -0700

    configures webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+// const webpack = require('webpack');
+
+module.exports = {
+  entry: path.join(__dirname, 'client', 'app.js'),
+  output: { path: path.join(__dirname, 'client'), filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2016', 'react'],
+        },
+      },
+    ],
+  },
+};
