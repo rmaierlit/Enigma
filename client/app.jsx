ae1d478447c66d6687a206b07f6c23f1cc82b3f2
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-09-02 16:29:27 -0700

    Replaces react-toolbox with material-ui
    moves state to Main component down from App
    resolves styling issues inherent in react-toolbox design
    --avoids issues with webpack compiling sass styles

--- client/app.jsx
+++ client/app.jsx
@@ -1,44 +1,20 @@
 /* eslint-env browser */
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Main from './components/Main';
-import CryptoJS from 'crypto-js';
 
-function encrypt(text, password) {
-  const ciphertext = CryptoJS.AES.encrypt(text, password);
-  return ciphertext;
-}
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { message: '', expDate: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleEncrypt = this.handleEncrypt.bind(this);
-  }
-
-  handleChange(value, event) {
-    const name = event.target.name;
-    this.setState({ [name]: value });
-  }
-
-  handleEncrypt() {
-    alert(encrypt(this.state.message, 'lolwut'));
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Father, give me legs!</h1>
-        <Main
-          handleChange={this.handleChange}
-          handleEncrypt={this.handleEncrypt}
-          message={this.state.message}
-          expDate={this.state.expDate}
-        />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <MuiThemeProvider>
+      <Main />
+    </MuiThemeProvider>
+  );
 }
 
 render(React.createElement(App), document.getElementById('app'));
--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,38 +1,72 @@
-import React from 'react';
-import { Card, CardTitle, CardActions } from 'react-toolbox/lib/card';
-import Button from 'react-toolbox/lib/button';
-import Input from 'react-toolbox/lib/input';
-import DatePicker from 'react-toolbox/lib/date_picker';
+/* eslint-env browser */
+import React, { Component } from 'react';
+import { Card, CardTitle, CardActions } from 'material-ui/Card';
+import Button from 'material-ui/FlatButton';
+import Input from 'material-ui/TextField';
+import DatePicker from 'material-ui/DatePicker';
+import CryptoJS from 'crypto-js';
 
-const Main = props => (
-  <Card style={{ width: '350px', padding: '10px' }}>
-    <CardTitle
-      title="Robert's Enigma"
-    />
-    <Input
-      type="text"
-      label="Message"
-      name="message"
-      required
-      maxLength={120}
-      value={props.message}
-      onChange={props.handleChange}
-    />
+function encrypt(text, password) {
+  const ciphertext = CryptoJS.AES.encrypt(text, password);
+  return ciphertext;
+}
 
-    <DatePicker
-      label="Expiration Date"
-      name="expDate"
-      onChange={props.handleChange}
-      value={props.expDate}
-      sundayFirstDayOfWeek
-    />
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { message: '', expDate: null };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleEncrypt = this.handleEncrypt.bind(this);
+  }
 
-    <CardActions>
-      <Button label="Encrypt" onClick={props.handleEncrypt} />
-      <Button label="Decrypt" />
-    </CardActions>
+  handleChange(event, value) {
+    const { name } = event.target;
+    this.setState({ [name]: value });
+  }
 
-  </Card>
-);
+  handleDateChange(ignore, value) {
+    this.setState({ expDate: value });
+  }
 
+  handleEncrypt() {
+    alert(encrypt(this.state.message, 'lolwut'));
+  }
+
+  render() {
+    return (
+      <Card style={{ width: '350px', padding: '10px' }}>
+        <CardTitle
+          title="Robert's Enigma"
+        />
+        <Input
+          type="text"
+          floatingLabelFixed
+          floatingLabelText="Message"
+          hintText="(required)"
+          name="message"
+          multiLine
+          maxLength={120}
+          value={this.state.message}
+          onChange={this.handleChange}
+        />
+
+        <DatePicker
+          floatingLabelFixed
+          floatingLabelText="Expiration Date"
+          hintText="(required)"
+          id="expDate"
+          onChange={this.handleDateChange}
+          value={this.state.expDate}
+        />
+
+        <CardActions>
+          <Button label="Encrypt" onClick={this.handleEncrypt} />
+          <Button label="Decrypt" />
+        </CardActions>
+
+      </Card>
+    );
+  }
+}
 export default Main;
