04935a36a12286f491b8c70da596c7a573dc32b6
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-09-04 23:40:39 -0700

    adds color and ok button to server message dialog

--- client/components/ServerDialog.jsx
+++ client/components/ServerDialog.jsx
@@ -4,9 +4,20 @@ import Button from 'material-ui/FlatButton';
 
 // displays messages from the server (such as when tablet is expired or cannot be decrypted)
 function ServerDialog(props) {
+  const actions = [
+    <Button
+      label="Ok"
+      onClick={props.handleClose}
+    />,
+  ];
+
   return (
-    <Dialog open={props.open} onRequestClose={props.handleClose}>
-      <h2>{props.message}</h2>
+    <Dialog
+      actions={actions}
+      open={props.open}
+      onRequestClose={props.handleClose}
+    >
+      <h2 style={{ color: 'rgb(244, 67, 54)' }}>{props.message}</h2>
     </Dialog>
   );
 }
